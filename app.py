fd8013ad085b0bd696125686536d18d8c5fa4658
SmartFarming50 <smartirrigation2026@gmail.com>
2026-01-26 14:20:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -209,4 +209,7 @@ def qr_image(qr_code):
 # START
 # --------------------------------------------------
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+
